4fb0ecd35450381edf80219f3bf76c46d50f0f12
Vita Dovhan <87687287+Vita-Dovhan@users.noreply.github.com>
2022-04-08 19:49:58 +0300

    correct

--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
-import './sass/main.scss';
-const refs = {
-  openBtn: document.querySelector('[data-action="open-modal"]'),
-  backdrop: document.querySelector('.js-backdrop'),
-  closeBtn: document.querySelector('[data-action="close-modal"]')
-}
-refs.openBtn.addEventListener("click", onOpenModal)
-refs.backdrop.addEventListener("click", onBackdrop)
-refs.closeBtn.addEventListener("click", onCloseModal)
+// import './sass/main.scss';
+// const refs = {
+//   openBtn: document.querySelector('[data-action="open-modal"]'),
+//   backdrop: document.querySelector('.js-backdrop'),
+//   closeBtn: document.querySelector('[data-action="close-modal"]')
+// }
+// refs.openBtn.addEventListener("click", onOpenModal)
+// refs.backdrop.addEventListener("click", onBackdrop)
+// refs.closeBtn.addEventListener("click", onCloseModal)
 
 
-function onOpenModal() {
-  window.addEventListener('keydown', onEscPress);
-  document.body.classList.add('show-modal')
-}
+// function onOpenModal() {
+//   window.addEventListener('keydown', onEscPress);
+//   document.body.classList.add('show-modal')
+// }
 
-function onCloseModal() {
-  window.removeEventListener('keydown', onEscPress)
-  document.body.classList.remove('show-modal')
-}
-function onBackdrop(event) {
-  if (event.currentTarget === event.target) {
-    onCloseModal();
-  }
-}
-function onEscPress(event) {
-  if (event.code === 'Escape') {
-    onCloseModal();
-  }
-}
+// function onCloseModal() {
+//   window.removeEventListener('keydown', onEscPress)
+//   document.body.classList.remove('show-modal')
+// }
+// function onBackdrop(event) {
+//   if (event.currentTarget === event.target) {
+//     onCloseModal();
+//   }
+// }
+// function onEscPress(event) {
+//   if (event.code === 'Escape') {
+//     onCloseModal();
+//   }
+// }
+
+
+const x = 1;
+const x = 'hello';
+console.log(x);
